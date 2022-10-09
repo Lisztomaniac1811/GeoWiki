4b864109c090ca189bd387bda98943d3587f7749
Robert <robert.kowalski12000@gmail.com>
2022-10-09 10:45:02 +0400

    added #106

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 // General Wiki Variables
 var WikiTitle = "ბეიტსის მოტელის ვიკი";
-var PageCount = "5";
+var PageCount = "6";
 var WordmarkURL = "images/Wiki-wordmark.png";
 var Author = "Lisztomaniac";
 var CreationYear = "2022";
