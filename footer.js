544436aece9bb6a6c7765496b949c4e5331f6389
Robert <sanikidze.luca@gmail.com>
2023-04-11 20:55:05 +0400

    Update footer.js

--- footer.js
+++ footer.js
@@ -7,74 +7,94 @@ const searchbar = document.getElementById("search-bar");
 const searchresults = document.getElementById("search-results");
 
 const items = [
-  {name: NavItem111, preview: "101 Promo (35).jpg", description: "პირველი სეზონის პირველი ეპიზოდი.", link: NavItem111URL},
-  {name: NavItem112, preview: "102 Promo (34).jpg", description: "პირველი სეზონის მეორე ეპიზოდი.", link: NavItem112URL},
-  {name: NavItem113, preview: "103 Promo (13).jpg", description: "პირველი სეზონის მესამე ეპიზოდი.", link: NavItem113URL},
-  {name: NavItem114, preview: "104 Promo (7).jpg", description: "პირველი სეზონის მეოთხე ეპიზოდი.", link: NavItem114URL},
-  {name: NavItem115, preview: "105 Promo (13).jpg", description: "პირველი სეზონის მეხუთე ეპიზოდი.", link: NavItem115URL},
-  {name: NavItem116, preview: "106 Promo (8).jpg", description: "პირველი სეზონის მეექვსე ეპიზოდი.", link: NavItem116URL},
-  {name: NavItem117, preview: "107 Promo (22).jpg", description: "პირველი სეზონის მეშვიდე ეპიზოდი.", link: NavItem117URL},
-  {name: NavItem118, preview: "108 Promo (21).jpg", description: "პირველი სეზონის მერვე ეპიზოდი.", link: NavItem118URL},
-  {name: NavItem119, preview: "109 Promo (1).jpg", description: "პირველი სეზონის მეცხრე ეპიზოდი.", link: NavItem119URL},
-  {name: NavItem1110, preview: "110 Promo (16).jpg", description: "პირველი სეზონის მეათე ეპიზოდი.", link: NavItem1110URL},
-  {name: NavItem121, preview: "201 Promo (1).jpg", description: "მეორე სეზონის პირველი ეპიზოდი.", link: NavItem121URL},
-  {name: NavItem122, preview: "202 Promo (19).jpg", description: "მეორე სეზონის მეორე ეპიზოდი.", link: NavItem122URL},
-  {name: NavItem123, preview: "203 Promo (4).jpg", description: "მეორე სეზონის მესამე ეპიზოდი.", link: NavItem123URL},
-  {name: NavItem124, preview: "204 Promo (10).jpg", description: "მეორე სეზონის მეოთხე ეპიზოდი.", link: NavItem124URL},
-  {name: NavItem125, preview: "205 Promo (10).jpg", description: "მეორე სეზონის მეხუთე ეპიზოდი.", link: NavItem125URL},
-  {name: NavItem126, preview: "206 Promo (7).jpg", description: "მეორე სეზონის მეექვსე ეპიზოდი.", link: NavItem126URL},
-  {name: NavItem127, preview: "207 Promo (13).jpg", description: "მეორე სეზონის მეშვიდე ეპიზოდი.", link: NavItem127URL},
-  {name: NavItem128, preview: "208 Promo (8).jpg", description: "მეორე სეზონის მერვე ეპიზოდი.", link: NavItem128URL},
-  {name: NavItem129, preview: "209 Promo (8).jpg", description: "მეორე სეზონის მეცხრე ეპიზოდი.", link: NavItem129URL},
-  {name: NavItem1210, preview: "210 Promo (14).jpg", description: "მეორე სეზონის მეათე ეპიზოდი.", link: NavItem1210URL},
-  {name: NavItem131, preview: "50x50.png", description: "მესამე სეზონის პირველი ეპიზოდი.", link: NavItem131URL},
-  {name: NavItem132, preview: "50x50.png", description: "მესამე სეზონის მეორე ეპიზოდი.", link: NavItem132URL},
-  {name: NavItem133, preview: "50x50.png", description: "მესამე სეზონის მესამე ეპიზოდი.", link: NavItem133URL},
-  {name: NavItem134, preview: "50x50.png", description: "მესამე სეზონის მეოთხე ეპიზოდი.", link: NavItem134URL},
-  {name: NavItem135, preview: "50x50.png", description: "მესამე სეზონის მეხუთე ეპიზოდი.", link: NavItem135URL},
-  {name: NavItem136, preview: "50x50.png", description: "მესამე სეზონის მეექვსე ეპიზოდი.", link: NavItem136URL},
-  {name: NavItem137, preview: "50x50.png", description: "მესამე სეზონის მეშვიდე ეპიზოდი.", link: NavItem137URL},
-  {name: NavItem138, preview: "50x50.png", description: "მესამე სეზონის მერვე ეპიზოდი.", link: NavItem138URL},
-  {name: NavItem139, preview: "50x50.png", description: "მესამე სეზონის მეათე ეპიზოდი.", link: NavItem139URL},
-  {name: NavItem1310, preview: "50x50.png", description: "მესამე სეზონის  ეპიზოდი.", link: NavItem1310URL},
-  {name: NavItem141, preview: "50x50.png", description: "მეოთხე სეზონის პირველი ეპიზოდი.", link: NavItem141URL},
-  {name: NavItem142, preview: "50x50.png", description: "მეოთხე სეზონის მეორე ეპიზოდი.", link: NavItem142URL},
-  {name: NavItem143, preview: "50x50.png", description: "მეოთხე სეზონის მესამე ეპიზოდი.", link: NavItem143URL},
-  {name: NavItem144, preview: "50x50.png", description: "მეოთხე სეზონის მეოთხე ეპიზოდი.", link: NavItem144URL},
-  {name: NavItem145, preview: "50x50.png", description: "მეოთხე სეზონის მეხუთე ეპიზოდი.", link: NavItem145URL},
-  {name: NavItem146, preview: "50x50.png", description: "მეოთხე სეზონის მეექვსე ეპიზოდი.", link: NavItem146URL},
-  {name: NavItem147, preview: "50x50.png", description: "მეოთხე სეზონის მეშვიდე ეპიზოდი.", link: NavItem147URL},
-  {name: NavItem148, preview: "50x50.png", description: "მეოთხე სეზონის მერვე ეპიზოდი.", link: NavItem148URL},
-  {name: NavItem149, preview: "50x50.png", description: "მეოთხე სეზონის მეცხრე ეპიზოდი.", link: NavItem149URL},
-  {name: NavItem1410, preview: "50x50.png", description: "მეოთხე სეზონის მეათე ეპიზოდი.", link: NavItem1410URL},
-  {name: NavItem151, preview: "50x50.png", description: "მეხუთე სეზონის პირველი ეპიზოდი.", link: NavItem151URL},
-  {name: NavItem152, preview: "50x50.png", description: "მეხუთე სეზონის მეორე ეპიზოდი.", link: NavItem152URL},
-  {name: NavItem153, preview: "50x50.png", description: "მეხუთე სეზონის მესამე ეპიზოდი.", link: NavItem153URL},
-  {name: NavItem154, preview: "50x50.png", description: "მეხუთე სეზონის მეოთხე ეპიზოდი.", link: NavItem154URL},
-  {name: NavItem155, preview: "50x50.png", description: "მეხუთე სეზონის მეხუთე ეპიზოდი.", link: NavItem155URL},
-  {name: NavItem156, preview: "50x50.png", description: "მეხუთე სეზონის მეექვსე ეპიზოდი.", link: NavItem156URL},
-  {name: NavItem157, preview: "50x50.png", description: "მეხუთე სეზონის მეშვიდე ეპიზოდი.", link: NavItem157URL},
-  {name: NavItem158, preview: "50x50.png", description: "მეხუთე სეზონის მერვე ეპიზოდი.", link: NavItem158URL},
-  {name: NavItem159, preview: "50x50.png", description: "მეხუთე სეზონის მეცხრე ეპიზოდი.", link: NavItem159URL},
-  {name: NavItem1510, preview: "50x50.png", description: "მეხუთე სეზონის მეათე ეპიზოდი.", link: NavItem1510URL},
-  {name: "ნორმან ბეიტსი", preview: "Norman.jpg", description: "ბეიტსის მოტელის მფლობელი. ნორმა ბეიტსის ვაჟი და დილან მასეტის ძმა.", link: "Norman_Bates.html"},
-  {name: "ნორმა ბეიტსი", preview: "Norma.jpg", description: "ბეიტსის მოტელის მფლობელი. ნორმან ბეიტსისა და დილან მასეტის დედა.", link: "Norma_Bates.html"},
-  {name: "დილან მასეტი", preview: "Dylan.jpg", description: "ნორმა ბეიტსის ვაჟი და ნორმან ბეიტსის ძმა.", link: "Dylan_Massett.html"},
-  {name: "ემა დეკოდი", preview: "Emma.jpg", description: "ნორმან ბეიტსის თანაკლასელი და საუკეთესო მეგობარი.", link: "Emma_Decody.html"},
-  {name: "ალექს რომერო", preview: "Romero.jpg", description: "ქალაქ თეთრი ფიჭვების ყურის შერიფი.", link: "Alex_Romero.html"},
+  {name: NavItem111, keywords: ["First You Dream, Then You Die"], preview: "101 Promo (35).jpg", description: "პირველი სეზონის პირველი ეპიზოდი.", link: NavItem111URL},
+  {name: NavItem112, keywords: ["Nice Town You Picked, Norma"], preview: "102 Promo (34).jpg", description: "პირველი სეზონის მეორე ეპიზოდი.", link: NavItem112URL},
+  {name: NavItem113, keywords: [""], preview: "103 Promo (13).jpg", description: "პირველი სეზონის მესამე ეპიზოდი.", link: NavItem113URL},
+  {name: NavItem114, keywords: [""], preview: "104 Promo (7).jpg", description: "პირველი სეზონის მეოთხე ეპიზოდი.", link: NavItem114URL},
+  {name: NavItem115, keywords: [""], preview: "105 Promo (13).jpg", description: "პირველი სეზონის მეხუთე ეპიზოდი.", link: NavItem115URL},
+  {name: NavItem116, keywords: [""], preview: "106 Promo (8).jpg", description: "პირველი სეზონის მეექვსე ეპიზოდი.", link: NavItem116URL},
+  {name: NavItem117, keywords: [""], preview: "107 Promo (22).jpg", description: "პირველი სეზონის მეშვიდე ეპიზოდი.", link: NavItem117URL},
+  {name: NavItem118, keywords: [""], preview: "108 Promo (21).jpg", description: "პირველი სეზონის მერვე ეპიზოდი.", link: NavItem118URL},
+  {name: NavItem119, keywords: [""], preview: "109 Promo (1).jpg", description: "პირველი სეზონის მეცხრე ეპიზოდი.", link: NavItem119URL},
+  {name: NavItem1110, keywords: [""], preview: "110 Promo (16).jpg", description: "პირველი სეზონის მეათე ეპიზოდი.", link: NavItem1110URL},
+  {name: NavItem121, keywords: [""], preview: "201 Promo (1).jpg", description: "მეორე სეზონის პირველი ეპიზოდი.", link: NavItem121URL},
+  {name: NavItem122, keywords: [""], preview: "202 Promo (19).jpg", description: "მეორე სეზონის მეორე ეპიზოდი.", link: NavItem122URL},
+  {name: NavItem123, keywords: [""], preview: "203 Promo (4).jpg", description: "მეორე სეზონის მესამე ეპიზოდი.", link: NavItem123URL},
+  {name: NavItem124, keywords: [""], preview: "204 Promo (10).jpg", description: "მეორე სეზონის მეოთხე ეპიზოდი.", link: NavItem124URL},
+  {name: NavItem125, keywords: [""], preview: "205 Promo (10).jpg", description: "მეორე სეზონის მეხუთე ეპიზოდი.", link: NavItem125URL},
+  {name: NavItem126, keywords: [""], preview: "206 Promo (7).jpg", description: "მეორე სეზონის მეექვსე ეპიზოდი.", link: NavItem126URL},
+  {name: NavItem127, keywords: [""], preview: "207 Promo (13).jpg", description: "მეორე სეზონის მეშვიდე ეპიზოდი.", link: NavItem127URL},
+  {name: NavItem128, keywords: [""], preview: "208 Promo (8).jpg", description: "მეორე სეზონის მერვე ეპიზოდი.", link: NavItem128URL},
+  {name: NavItem129, keywords: [""], preview: "209 Promo (8).jpg", description: "მეორე სეზონის მეცხრე ეპიზოდი.", link: NavItem129URL},
+  {name: NavItem1210, keywords: [""], preview: "210 Promo (14).jpg", description: "მეორე სეზონის მეათე ეპიზოდი.", link: NavItem1210URL},
+  {name: NavItem131, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის პირველი ეპიზოდი.", link: NavItem131URL},
+  {name: NavItem132, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მეორე ეპიზოდი.", link: NavItem132URL},
+  {name: NavItem133, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მესამე ეპიზოდი.", link: NavItem133URL},
+  {name: NavItem134, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მეოთხე ეპიზოდი.", link: NavItem134URL},
+  {name: NavItem135, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მეხუთე ეპიზოდი.", link: NavItem135URL},
+  {name: NavItem136, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მეექვსე ეპიზოდი.", link: NavItem136URL},
+  {name: NavItem137, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მეშვიდე ეპიზოდი.", link: NavItem137URL},
+  {name: NavItem138, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მერვე ეპიზოდი.", link: NavItem138URL},
+  {name: NavItem139, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის მეათე ეპიზოდი.", link: NavItem139URL},
+  {name: NavItem1310, keywords: [""], preview: "50x50.png", description: "მესამე სეზონის  ეპიზოდი.", link: NavItem1310URL},
+  {name: NavItem141, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის პირველი ეპიზოდი.", link: NavItem141URL},
+  {name: NavItem142, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მეორე ეპიზოდი.", link: NavItem142URL},
+  {name: NavItem143, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მესამე ეპიზოდი.", link: NavItem143URL},
+  {name: NavItem144, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მეოთხე ეპიზოდი.", link: NavItem144URL},
+  {name: NavItem145, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მეხუთე ეპიზოდი.", link: NavItem145URL},
+  {name: NavItem146, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მეექვსე ეპიზოდი.", link: NavItem146URL},
+  {name: NavItem147, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მეშვიდე ეპიზოდი.", link: NavItem147URL},
+  {name: NavItem148, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მერვე ეპიზოდი.", link: NavItem148URL},
+  {name: NavItem149, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მეცხრე ეპიზოდი.", link: NavItem149URL},
+  {name: NavItem1410, keywords: [""], preview: "50x50.png", description: "მეოთხე სეზონის მეათე ეპიზოდი.", link: NavItem1410URL},
+  {name: NavItem151, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის პირველი ეპიზოდი.", link: NavItem151URL},
+  {name: NavItem152, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მეორე ეპიზოდი.", link: NavItem152URL},
+  {name: NavItem153, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მესამე ეპიზოდი.", link: NavItem153URL},
+  {name: NavItem154, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მეოთხე ეპიზოდი.", link: NavItem154URL},
+  {name: NavItem155, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მეხუთე ეპიზოდი.", link: NavItem155URL},
+  {name: NavItem156, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მეექვსე ეპიზოდი.", link: NavItem156URL},
+  {name: NavItem157, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მეშვიდე ეპიზოდი.", link: NavItem157URL},
+  {name: NavItem158, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მერვე ეპიზოდი.", link: NavItem158URL},
+  {name: NavItem159, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მეცხრე ეპიზოდი.", link: NavItem159URL},
+  {name: NavItem1510, keywords: [""], preview: "50x50.png", description: "მეხუთე სეზონის მეათე ეპიზოდი.", link: NavItem1510URL},
+  {name: "ნორმან ბეიტსი", keywords: ["Norman Bates"], preview: "Norman.jpg", description: "ბეიტსის მოტელის მფლობელი. ნორმა ბეიტსის ვაჟი და დილან მასეტის ძმა.", link: "Norman_Bates.html"},
+  {name: "ნორმა ბეიტსი", keywords: ["Norma Bates"], preview: "Norma.jpg", description: "ბეიტსის მოტელის მფლობელი. ნორმან ბეიტსისა და დილან მასეტის დედა.", link: "Norma_Bates.html"},
+  {name: "დილან მასეტი", keywords: ["Dylan Massett"], preview: "Dylan.jpg", description: "ნორმა ბეიტსის ვაჟი და ნორმან ბეიტსის ძმა.", link: "Dylan_Massett.html"},
+  {name: "ემა დეკოდი", keywords: ["Emma Decody"], preview: "Emma.jpg", description: "ნორმან ბეიტსის თანაკლასელი და საუკეთესო მეგობარი.", link: "Emma_Decody.html"},
+  {name: "ალექს რომერო", keywords: ["Alex Romero"], preview: "Romero.jpg", description: "ქალაქ თეთრი ფიჭვების ყურის შერიფი.", link: "Alex_Romero.html"},
 ];
 
 function searchfunction() {
-  const term = searchbar.value.toLowerCase().trim();
+  const term = searchbar.value.toLowerCase().trim().replace(/[^\w\sა-ჰ]/g, "");
   if (term.length === 0) {
     searchresults.style.display = "none";
     searchresults.innerHTML = "";
   } else {
-    const filteredItems = items.filter(item => item.name.toLowerCase().includes(term));
+    const filteredItems = items.filter(item => {
+      const matchName = item.name.toLowerCase().replace(/[^\w\sა-ჰ]/g, "").includes(term);
+      const matchKeywords = item.keywords.some(keyword => keyword.toLowerCase().replace(/[^\w\sა-ჰ]/g, "").includes(term));
+      return matchName || matchKeywords;
+    });
     if (filteredItems.length === 0) {
       searchresults.style.display = "none";
       searchresults.innerHTML = "";
     } else {
+      filteredItems.sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        const keywordA = a.keywords.join("").toLowerCase();
+        const keywordB = b.keywords.join("").toLowerCase();
+        if (nameA.includes(term)) {
+          return -1;
+        } else if (nameB.includes(term)) {
+          return 1;
+        } else if (keywordA.includes(term)) {
+          return -1;
+        } else if (keywordB.includes(term)) {
+          return 1;
+        }
+        return 0;
+      });
       searchresults.style.display = "block";
       searchresults.innerHTML = "";
       for (const item of filteredItems) {
@@ -89,6 +109,9 @@ function searchfunction() {
     }
   }
 }
+
+
+
 
 searchbar.addEventListener('input', function() {
 if (searchbar.value.trim() !== '') {
