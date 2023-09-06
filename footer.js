a3e13e05ce27a3801fa92cdf7bd44f8a9e538af6
Robert <sanikidze.luca@gmail.com>
2023-09-06 09:48:45 +0400

    udpate

--- footer.js
+++ footer.js
@@ -168,7 +168,20 @@ window.addEventListener('scroll', function() {
 });
 
 
-
+document.write(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="height: 0px; width: 0px; position: absolute; overflow: hidden;" aria-hidden="true">
+<symbol id="wds-player-icon-play" viewbox="0 0 180 180">
+  <g fill="none" fill-rule="evenodd">
+    <!-- Add the circle behind the play button -->
+    <circle cx="90" cy="90" r="60" fill="rgba(255, 255, 255, 0.9)" \/>
+    <g opacity=".9" transform="rotate(90 75 90)">
+      <use xlink:href="#b" fill="#000" filter="url(#a)" \/>
+      <use xlink:href="#b" fill="#FFF" \/>
+    <\/g>
+    <path fill="#fa005a" fill-rule="nonzero" d="M80.87 58.006l34.32 25.523c3.052 2.27 3.722 6.633 1.496 9.746a6.91 6.91 0 0 1-1.497 1.527l-34.32 25.523c-3.053 2.27-7.33 1.586-9.558-1.527A7.07 7.07 0 0 1 70 114.69V63.643c0-3.854 3.063-6.977 6.84-6.977 1.45 0 2.86.47 4.03 1.34z" \/>
+  <\/g>
+<\/symbol>
+<\/svg>
+`)
 // const links = document.getElementsByTagName('a');
 // for (let i = 0; i < links.length; i++) {
 //   const link = links[i];
